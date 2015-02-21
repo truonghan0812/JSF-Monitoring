56670048b108404997557b0b63ed9f5ad8c755a9
TRUONG HAN <TRUONG HAN@TRUONGHAN-PC>
2015-02-21 11:03:17 +0700

    hello world

---
+++ SiteMonitoring-Web/src/main/java/com/truonghan/sitemonitoring/web/beans/Controller.java
@@ -0,0 +1,13 @@
+package com.truonghan.sitemonitoring.web.beans;
+
+import javax.faces.bean.ManagedBean;
+
+
+
+@ManagedBean
+public class Controller {
+
+	public String index(){
+		return "Hello world";
+	}
+}
